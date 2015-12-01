4491faf598b8fde01bf93788e8030e43b3efaa3e
Steve Barnett <steve@naga.co.za>
2015-12-01 13:12:47 +0200

    Add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-  // place code for your default task here  
+gulp.task('lint', function() {
+  return gulp.src('./js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', ['lint'], function() {
 });
