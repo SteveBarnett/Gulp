67de1b9fdf74a9dc8c8e8407216274e827b91dfd
Steve Barnett <steve@naga.co.za>
2015-12-01 17:44:01 +0200

    Tidy up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,67 +11,79 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     concatcss = require('gulp-concat-css');
 
-gulp.task('lint', function() {
-  return gulp.src(['./js/*.js', '!./js/*.min.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+// HTML
 
-gulp.task('uglify', function() {
-    gulp.src(['./js/*.js', '!./js/*.min.js'])
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(rename({
-          extname: '.min.js'
-        }))
-        .pipe(gulp.dest('./js'));
-});
-
-gulp.task('jscs', function() {
-  return gulp.src(['./js/*.js', '!./js/*.min.js'])
-      .pipe(jscs({fix: true}))
-      .pipe(jscs())
-      .pipe(jscs.reporter())
-      .pipe(gulp.dest('./js'));
-});
-
-gulp.task('access', function() {
-  return gulp.src('./*.html')
-    .pipe(access(
-      {
-        accessibilityLevel: 'WCAG2AAA'
-      }
-    ));
-});
-
-gulp.task('htmlhint', function() {
+gulp.task('html:hint', function() {
   return gulp.src("./*.html")
     .pipe(htmlhint())
     .pipe(htmlhint.reporter())
 });
 
-gulp.task('sass', function () {
-  gulp.src('./sass/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+gulp.task('html:accessibility', function() {
+  return gulp.src('./*.html')
+    .pipe(
+      access({accessibilityLevel: 'WCAG2AAA'})
+    );
 });
 
-gulp.task('csslint', function () {
+gulp.task('html', ['html:hint', 'html:accessibility']);
+
+// CSS
+
+gulp.task('css:lint', function () {
   return gulp.src(['./css/main.css', './css/normalise.css'])
     .pipe(csslint())
     .pipe(csslint.reporter())
 });
 
-gulp.task('concatandminfiycss', function () {
+gulp.task('css:concatandminify', function () {
   return gulp.src(['./css/main.css', './css/normalise.css'])
     .pipe(concatcss('bundle.css'))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
+// Sass
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// JavaScript
+
+gulp.task('js:lint', function() {
+  return gulp.src(['./js/*.js', '!./js/*.min.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('js:jscs', function() {
+  return gulp.src(['./js/*.js', '!./js/*.min.js'])
+    // .pipe(jscs({fix: true}))
+    .pipe(jscs())
+    .pipe(jscs.reporter())
+    .pipe(gulp.dest('./js'));
+});
+
+gulp.task('js:uglify', function() {
+  return gulp.src(['./js/*.js', '!./js/*.min.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./js'));
+});
+
+gulp.task('js', ['js:lint', 'js:jscs']);
+
+// gulp all the things
+
 gulp.task('default', function() {
-  gulp.watch('./js/*.js', ['lint', 'uglify']);
-  gulp.watch('./*.html', ['access', 'htmlhint']);
+  gulp.watch('./*.html', ['html']);
+  gulp.watch(['./css/main.css', './css/normalise.css'], ['css:lint']);
   gulp.watch('./sass/*.scss', ['sass']);
-  gulp.watch(['./css/main.css', './css/normalise.css', '!./css/style.css'], ['csslint', 'concatandminfiycss']);
+  gulp.watch('./js/*.js', ['js:lint', 'js:jscs']);
 });
