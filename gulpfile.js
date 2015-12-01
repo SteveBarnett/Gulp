49ad296136d84501e07e8341c388ceb978d2c762
Steve Barnett <steve@naga.co.za>
2015-12-01 13:26:37 +0200

    Add uglify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename"),
+    concat = require("gulp-concat");
 
 gulp.task('lint', function() {
-  return gulp.src('./js/*.js')
+  return gulp.src(['./js/*.js', '!./js/*.min.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
+gulp.task('uglify', function() {
+    gulp.src(['./js/*.js', '!./js/*.min.js'])
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(rename({
+          extname: '.min.js'
+        }))
+        .pipe(gulp.dest('./js'));
+});
+
 gulp.task('default', function() {
-  gulp.watch('./js/*.js', ['lint']);
+  gulp.watch('./js/*.js', ['lint', 'uglify']);
 });
