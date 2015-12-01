7829e2cf1a9a55d0e66fbf2ba772772193730c0c
Steve Barnett <steve@naga.co.za>
2015-12-01 15:13:44 +0200

    Add accessibility checker

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require("gulp-rename"),
     concat = require("gulp-concat"),
-    jscs = require("gulp-jscs");
+    jscs = require("gulp-jscs"),
+    access = require('gulp-accessibility');
 
 gulp.task('lint', function() {
   return gulp.src(['./js/*.js', '!./js/*.min.js'])
@@ -29,7 +30,16 @@ gulp.task('jscs', function() {
       .pipe(gulp.dest('./js'));
 });
 
+gulp.task('access', function() {
+  return gulp.src('./*.html')
+    .pipe(access(
+      {
+        accessibilityLevel: 'WCAG2AAA'
+      }
+    ));
+});
 
 gulp.task('default', function() {
   gulp.watch('./js/*.js', ['lint', 'uglify']);
+  gulp.watch('./*.html', ['access']);
 });
