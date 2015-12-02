519d74a5c1ba45dec63febb85be7236c43a0b328
Steve Barnett <steve@naga.co.za>
2015-12-02 07:06:44 +0200

    Move targets to config

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,26 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
 
+var config = {
+  html: './*.html',
+  csslint: ['./css/main.css', '!./css/h5bp.css', '!./css/src/normalize.css'],
+  cssconcatandminify: ['./css/*.css'],
+  sass: './sass/*.scss',
+  js: ['./js/*.js', '!./js/*.min.js']
+};
+
 // HTML
 
 // hint
 gulp.task('html:hint', function() {
-  return gulp.src('./*.html')
+  return gulp.src(config.html)
     .pipe(htmlhint())
     .pipe(htmlhint.reporter())
 });
 
 // accessibility
 gulp.task('html:accessibility', function() {
-  return gulp.src('./*.html')
+  return gulp.src('config.html')
     .pipe(
       access({accessibilityLevel: 'WCAG2AAA'})
     );
@@ -32,14 +40,14 @@ gulp.task('html:accessibility', function() {
 
 // lint
 gulp.task('css:lint', function () {
-  return gulp.src(['./css/main.css', '!./css/h5bp.css', '!./css/src/normalize.css'])
+  return gulp.src(config.csslint)
     .pipe(csslint())
     .pipe(csslint.reporter())
 });
 
 // concat and minify
 gulp.task('css:concatandminify', function () {
-  return gulp.src(['./css/*.css'])
+  return gulp.src(config.cssconcatandminify)
     .pipe(concatcss('style.min.css'))
     .pipe(minifycss())
     .pipe(gulp.dest('./css'));
@@ -48,7 +56,7 @@ gulp.task('css:concatandminify', function () {
 // Sass
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+  return gulp.src(config.sass)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({
       extname: '.min.css'
@@ -60,14 +68,14 @@ gulp.task('sass', function () {
 
 // lint
 gulp.task('js:lint', function() {
-  return gulp.src(['./js/*.js', '!./js/*.min.js'])
+  return gulp.src(config.js)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 // jscs
 gulp.task('js:jscs', function() {
-  return gulp.src(['./js/*.js', '!./js/*.min.js'])
+  return gulp.src(config.js)
     // .pipe(jscs({fix: true}))
     .pipe(jscs())
     .pipe(jscs.reporter());
@@ -76,7 +84,7 @@ gulp.task('js:jscs', function() {
 
 // uglify
 gulp.task('js:uglify', function() {
-  return gulp.src(['./js/*.js', '!./js/*.min.js'])
+  return gulp.src(config.js)
     .pipe(concat('main.js'))
     .pipe(uglify())
     .pipe(rename({
@@ -86,7 +94,7 @@ gulp.task('js:uglify', function() {
 });
 
 // lint all the things
-gulp.task('lint', function() {
+gulp.task('default', function() {
   gulp.watch('./*.html', ['html:hint', 'html:accessibility']);
   gulp.watch(['./css/*.css'], ['css:lint']);
   gulp.watch('./js/*.js', ['js:lint', 'js:jscs']);
